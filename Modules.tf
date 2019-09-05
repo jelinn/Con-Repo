8d57d98aaa3106310850326c99de54b0e0bbae2b
jelinn <jelinn@gmail.com>
2019-09-04 22:33:07 -0500

    Update Modules.tf

--- Modules.tf
+++ Modules.tf
@@ -1,17 +1,16 @@
-module "consumer_ec2_instance_1" {
-  source  = "app.terraform.io/jlinn/consumer-ec2-instance-1/aws"
-  version = "1.0.0"
+module "consumer_ec2_instance" {
+  source  = "app.terraform.io/jlinn/consumer-ec2-instance/aws"
+  version = "1.0"
 
   instance_count = 4
   name = "App1"
 }
 
-/*module "consumer_elb_1" {
-  source  = "app.terraform.io/jlinn/consumer-elb-1/aws"
-  version = "1.0.0"
+module "consumer_elb" {
+  source  = "app.terraform.io/jlinn/consumer-elb/aws"
+  version = "1.0"
 
   environment = "dev"
-  instances = [module.consumer_ec2_instance_1.[*].id]
-  name = "Consumer-App1"
+  instances = "${module.consumer_ec2_instance.id}"
+  name = "App1"
 }
-*/
