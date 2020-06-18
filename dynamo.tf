1e4e6bc204eac970f9d5c15e52a2f41d33711255
jelinn <jelinn@gmail.com>
2020-06-18 08:01:33 -0500

    Update dynamo.tf

--- dynamo.tf
+++ dynamo.tf
@@ -1,6 +1,6 @@
 
 module "consumer_ec2_instance_1" {
-  source  = "app.terraform.io/JLOrg/consumer-ec2-instance-1/aws"
+  source  = "app.terraform.io/JLOrg/consumer-ec2-instance-1"
   version = "1.0.0"
 
   instance_count = 2
@@ -8,7 +8,7 @@ module "consumer_ec2_instance_1" {
 }
 
 module "consumer_elb_1" {
-  source  = "app.terraform.io/JLOrg/consumer-elb-1/aws"
+  source  = "app.terraform.io/JLOrg/consumer-elb-1"
   version = "1.0.0"
 
   environment = "dev"
