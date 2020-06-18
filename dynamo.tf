ea147e4a723d1ded9e82bce0bc5700ef72933040
jelinn <jelinn@gmail.com>
2020-06-18 07:56:54 -0500

    Update dynamo.tf

--- dynamo.tf
+++ dynamo.tf
@@ -1,5 +1,21 @@
 
+module "consumer_ec2_instance_1" {
+  source  = "app.terraform.io/JLOrg/consumer-ec2-instance-1/aws"
+  version = "1.0.0"
 
+  instance_count = 2
+  name = "app1"
+}
+
+module "consumer_elb_1" {
+  source  = "app.terraform.io/JLOrg/consumer-elb-1/aws"
+  version = "1.0.0"
+
+  environment = "dev"
+  instances = "${module.consumer_ec2_instance_1.id"
+  name = "app1"
+  number_of_instances = 2
+}
 resource "aws_dynamodb_table" "basic-dynamodb-table" {
   name           = "${var.org}-${var.environment}-GameScores"
   billing_mode   = "PROVISIONED"
